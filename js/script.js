d28ebee80f6ba6522fb9a82a8023ed19a1ccbcd0
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-09-03 16:39:46 +0300

    feat: add style for header screen 940px and burger menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log('Hi')
+(function openMenu() {
+  const burger = document.querySelector('.burger-menu');
+  const navbar = document.querySelector('.navbar');
+
+  burger.addEventListener('click', () => {
+    navbar.classList.toggle('navbar-active')
+    burger.classList.toggle('burger-menu_active');
+  });
+}())
