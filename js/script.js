6ce1db4b28cad6d7b3932ead04852318f9af4d98
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-09-04 10:47:58 +0300

    feat: add style for hero banner responsive

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-(function openMenu() {
+(function() {
+  const body = document.querySelector('body');
   const burger = document.querySelector('.burger-menu');
   const navbar = document.querySelector('.navbar');
 
   burger.addEventListener('click', () => {
+    body.classList.toggle('body-active')
     navbar.classList.toggle('navbar-active')
     burger.classList.toggle('burger-menu_active');
   });
