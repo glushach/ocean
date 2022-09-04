ad15bc58bf50e31c95b2909cc530fc92f225f7d3
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-09-04 16:59:05 +0300

    feat: add html and style for slide 1

--- js/script.js
+++ js/script.js
@@ -9,3 +9,46 @@
     burger.classList.toggle('burger-menu_active');
   });
 }())
+
+
+new Swiper(".swiper-first", {
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1.13,
+      spaceBetween: 22,
+    },
+    450: {
+      slidesPerView: 1.4,
+      spaceBetween: 25,
+    },
+    500: {
+      slidesPerView: 1.5,
+      spaceBetween: 25,
+    },
+    560: {
+      slidesPerView: 1.7,
+      spaceBetween: 33,
+    },
+    641: {
+      slidesPerView: 1.5,
+      spaceBetween: 33,
+    },
+    1051: {
+      slidesPerView: 2.1,
+      spaceBetween: 33,
+    },
+    1110: {
+      slidesPerView: 2.3,
+      spaceBetween: 33,
+    },
+    1210: {
+      slidesPerView: 2.5,
+      spaceBetween: 33,
+    },
+    1600: {
+      slidesPerView: 3,
+      spaceBetween: 33,
+    }
+  },
+});
