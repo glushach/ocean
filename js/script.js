342f8e4501a8c57ca2ca616c556b9c5d8ef20105
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-09-05 11:26:32 +0300

    feat: add adaptive style for slider 2

--- js/script.js
+++ js/script.js
@@ -11,58 +11,51 @@
 }())
 
 
-new Swiper(".swiper-first", {
-  loop: true,
-  breakpoints: {
-    300: {
-      slidesPerView: 0.95,
-      spaceBetween: 10,
-    },
-    375: {
-      slidesPerView: 1.05,
-    },
-    440: {
-      slidesPerView: 1.2,
-    },
-    540: {
-      slidesPerView: 1.4,
-    },
-    640: {
-      slidesPerView: 1.3,
-    },
-    870: {
-      slidesPerView: 1.6,
-    },
-    840: {
-      slidesPerView: 1.7,
-    },
-    1000: {
-      slidesPerView: 1.9,
-    },
-    1100: {
-      slidesPerView: 2.1,
-    },
-    1400: {
-      slidesPerView: 2.5,
-    },
-    1600: {
-      slidesPerView: 3,
-      spaceBetween: 33,
-    }
-  },
-});
-
-
-new Swiper(".swiper-second", {
-  loop: true,
-  breakpoints: {
+const breakpoints = {
   300: {
     slidesPerView: 0.95,
     spaceBetween: 10,
+  },
+  375: {
+    slidesPerView: 1.05,
+  },
+  440: {
+    slidesPerView: 1.2,
+  },
+  540: {
+    slidesPerView: 1.4,
+  },
+  640: {
+    slidesPerView: 1.3,
+  },
+  870: {
+    slidesPerView: 1.6,
+  },
+  840: {
+    slidesPerView: 1.7,
+  },
+  1000: {
+    slidesPerView: 1.9,
+  },
+  1100: {
+    slidesPerView: 2.1,
+  },
+  1400: {
+    slidesPerView: 2.5,
   },
   1600: {
     slidesPerView: 3,
     spaceBetween: 33,
   }
-  }
+}
+
+new Swiper(".swiper-first", {
+  loop: true,
+  breakpoints: breakpoints,
+});
+
+
+new Swiper(".swiper-second", {
+  loop: true,
+  breakpoints: breakpoints,
 });
